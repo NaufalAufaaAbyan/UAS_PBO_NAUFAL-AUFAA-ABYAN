47e0610377d6acbc6d2bd2d3bb911eadf3e0ae5d
LAB_UNAMA <LAB_UNAMA@DESKTOP-C3INA2C>
2025-07-26 11:50:44 +0700

    Add project files.

---
+++ Login.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAS_PBO_NAUFAL_AUFAA_ABYAN
+{
+    internal class Login
+    {
+        public string judulForm = "=====Login User=====";
+        public string username;
+        public string password;
+
+        public void tampilForm()
+        {
+            Console.SetCursorPosition((Console.WindowHeight - this.judulForm.Length) / 2, Console.CursorTop);
+            Console.WriteLine(this.judulForm);
+            Console.WriteLine();
+            Console.Write("UserName : "); this.username = Console.ReadLine();
+            Console.Write("Password : "); this.password = Console.ReadLine();
+        }
+
+        public bool apakahLoginBerhasil()
+        {
+            if (this.username=="admin" && this.password=="rahasia")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAS_PBO_NAUFAL_AUFAA_ABYAN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Login login = new Login();
+            login.tampilForm();
+            if(login.apakahLoginBerhasil())
+            {
+                Console.WriteLine("Login Berhasil. Selamat Datang, " + login.username);
+            }
+            else
+            {
+                Console.WriteLine("Login Gagal");
+            }
+            Console.ReadKey();
+        }
+    }
+}
